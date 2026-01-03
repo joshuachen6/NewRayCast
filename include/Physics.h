2ce36ecca062d831145324ec52ead057eb787aed
joshuachen6 <jhc3628@gmail.com>
2026-01-03 17:50:51 -0600

    Math optimization for ray cast

--- include/Physics.h
+++ include/Physics.h
@@ -13,11 +13,9 @@ const double METER = 100;
 
 class Physics {
 public:
-  static std::vector<CastResult>
-  cast_ray(World &world, const sf::Vector3f &source, double angle);
+  static std::vector<CastResult> cast_ray(World &world, const sf::Vector3f &source, double angle);
   static double scale_angle(double radians);
-  static bool hits_vertex(Vertex &vertex, sf::Vector2f &point,
-                          const sf::Vector3f &source, double angle);
+  static bool hits_vertex(Vertex &vertex, sf::Vector2f &point, const sf::Vector3f &source, double slope, sf::Vector2f lvector);
   static void apply_physics(World &world, double dt);
 
   static double dot(sf::Vector2f a, sf::Vector2f b);
